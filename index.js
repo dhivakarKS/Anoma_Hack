f07b1fcf5d48844ee0cce657d79a6a673e52ce40
Dharanesh K <DharanK@eu.trimblecorp.net>
2021-10-17 20:14:42 +0530

    check owner and actor

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 //Github Requirements
 const github  = require('@actions/github');                     //Github Module
+const core = require('@actions/core');
 const context = github.context                                  //Github contexts module(to get the payload and to post a comment)
-const myToken = "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";     //Personal github access token
+const myToken = core.getInput('myToken');                       //Personal github access token
 const octokit = github.getOctokit(myToken)                      //provides access to the GitHub API
 const core = require('@actions/core');                          //Get Data from action.yml file
 
 //Azure Cognitive Services Requirements:
 //
-
-const owner = 'dharanesh12899',repo = 'Client';
 
 
 const getData = async() =>{
@@ -21,21 +20,14 @@ const getData = async() =>{
     else {
         comment = context.payload.issue.body
     }
-    if(context.actor !== context.issue.owner){
+        console.log("Actor: "+context.actor)
+        console.log("Owner: "+context.issue.owner)
         const data = await octokit.issues.createComment({
             owner: context.issue.owner,
             repo: context.issue.repo,
             issue_number: context.issue.number,
             body: `Please delete this comment @${context.actor}`,
         }).then(console.log("comment added "+context.issue.owner));
-    }
-    else{
-        return 0
-    }
-    
-
-    /*const commits = await octokit.request({url,owner,repo,method:'Get',path:'contents'})
-    console.log(fiveMostRecentCommits[0].data)*/
 }
 
 getData();
