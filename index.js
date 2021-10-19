0b51572c4d78baf7eb13a9a6648b51c2a193ca8b
dharanesh12899 <dharun.k12899@gmail.com>
2021-10-19 16:34:55 +0530

    commit

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const commentIt = async() =>{
         for (link of links) {
             console.log(link);
             const desc = (await computerVisionClient.analyzeImage(link, {
-                visualFeatures: ['Adult','Description']
+                visualFeatures: ['Adult']
             })).desc;
             console.log(JSON.stringify(desc));
             //console.log(adult)
