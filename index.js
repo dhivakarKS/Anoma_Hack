89a5f1e6fbdeb94875570609234270e7dbb01e14
dharanesh12899 <dharun.k12899@gmail.com>
2021-10-21 15:54:12 +0530

    proper score print

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const commentIt = async() =>{
             const adult = (await computerVisionClient.analyzeImage(link, {
                 visualFeatures: ['Adult']
             })).adult;
-            console.log(adult.adultscore + 0.6)
+            console.log(adult.adultscore)
 
             if (adult.adultScore + 0.6 >= 0.6) {
                 const data = await octokit.issues.createComment({
